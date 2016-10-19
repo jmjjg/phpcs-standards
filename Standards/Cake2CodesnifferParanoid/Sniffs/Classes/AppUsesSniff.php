a59ee70c33390c32c42d3bdac92f51cd1d191c9f
jmjjg <jmjjg@users.noreply.github.com>
2016-10-20 00:26:30 +0200

    Removed unnecessary explicit PHP-only value for supportedTokenizers.

--- Standards/Cake2CodesnifferParanoid/Sniffs/Classes/AppUsesSniff.php
+++ Standards/Cake2CodesnifferParanoid/Sniffs/Classes/AppUsesSniff.php
@@ -12,13 +12,6 @@
 class Cake2CodesnifferParanoid_Sniffs_Classes_AppUsesSniff implements PHP_CodeSniffer_Sniff
 {
     /**
-     * A list of tokenizers this sniff supports.
-     *
-     * @var array
-     */
-    public $supportedTokenizers = array( 'PHP' );
-
-    /**
      * A list of currently available class names.
      *
      * @var array
