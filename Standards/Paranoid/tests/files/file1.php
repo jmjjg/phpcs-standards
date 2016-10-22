13c0ffaa08faec3a2a6932f99bb833eee1b8230a
jmjjg <jmjjg@users.noreply.github.com>
2016-10-22 13:51:03 +0200

    Updated the Paranoid standard's unit test example file with Generic.CodeAnalysis examples, updated unit tests expected file, added a code browser output in a new quality ant target.

--- Standards/Paranoid/tests/files/file1.php
+++ Standards/Paranoid/tests/files/file1.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Class to check the ParanoidCodeSnifferStandard CodeSniffer standard.
  */
 class Foo
 {
+
     /**
      * PSR2.Classes.PropertyDeclaration.VarUsed
      * PSR2.Classes.PropertyDeclaration.ScopeMissing
@@ -16,20 +18,64 @@ class Foo
      * Let's have some errors detected.
      *
      * Squiz.Scope.MethodScope.Missing
+     * Generic.CodeAnalysis.UnusedFunctionParameter
      */
-    function foo()
-    {
+    function foo($bar = array()) {
         // Generic.Formatting.NoSpaceAfterCast.SpaceFound
         $x = (array) NULL;
-        // Generic.CodeAnalysis
+
+        // Generic.CodeAnalysis.EmptyStatement
         if ($x === 5) {
 
         }
+
+        // Generic.CodeAnalysis.ForLoopShouldBeWhileLoop
+        for (; $x > 5;
+        ) {
+
+        }
+
+        // Generic.CodeAnalysis.ForLoopWithTestFunctionCall
+        $a = array(1, 2, 3, 4);
+        for ($i = 0; $i < count($a); $i++) {
+            $a[$i] *= $i;
+        }
+
+        // Generic.CodeAnalysis.JumbledIncrementer
+        for ($i = 0; $i < 10; $i++) {
+            for ($k = 0; $k < 20; $i++) {
+                echo 'Hello';
+            }
+        }
+
+        // Generic.CodeAnalysis.UnconditionalIfStatement
+        if (true) {
+
+        }
+
         // Squiz.Strings.EchoedStrings.HasBracket
         echo($x);
+
         // Squiz.Commenting.FunctionCommentThrowTag
         throw new RuntimeException();
+
         // PHPCompatibility.PHP.ForbiddenCallTimePassByReference.NotAllowed
         $this->foo(&$x);
     }
+
+}
+
+final class Bar
+{
+
+    // Generic.CodeAnalysis.UnnecessaryFinalModifier
+    final public function bar() {
+
+    }
+
+    // Generic.CodeAnalysis.UselessOverridingMethod
+    public function __construct($foo, $bar) {
+        parent::__construct($foo, $bar);
+    }
+
 }
