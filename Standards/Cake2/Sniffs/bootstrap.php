e23d60d525435be4e517a9ff139fcdcc258b9b63
jmjjg <jmjjg@users.noreply.github.com>
2016-10-27 00:03:08 +0200

    Added two adapted functions from CakePHP 2.9.0: debug and pluginSplit.

---
+++ Standards/Cake2/Sniffs/bootstrap.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Basic CakePHP functionality adaptation from CakePHP 2.9.0 source code.
+ *
+ * @author Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @author Christian Buffin
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @see http://cakephp.org
+ */
+define('ROOT', dirname(dirname(__FILE__)) . '/');
+
+/**
+ * CakePHP's debug basic function adaptation from CakePHP 2.9.0
+ *
+ * @param mixed $var Variable to show debug information for.
+ * @return void
+ * @see http://book.cakephp.org/2.0/en/development/debugging.html#basic-debugging
+ * @see http://book.cakephp.org/2.0/en/core-libraries/global-constants-and-functions.html#debug
+ */
+function debug($var, $showHtml = null)
+{
+    $lineInfo = '';
+
+    $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    $file = str_replace(array(ROOT), '', $trace[0]['file']);
+    $line = $trace[0]['line'];
+
+    $html = <<<HTML
+<div class="cake-debug-output">
+%s
+<pre class="cake-debug">
+%s
+</pre>
+</div>
+HTML;
+    $text = <<<TEXT
+%s
+########## DEBUG ##########
+%s
+###########################
+
+TEXT;
+
+    $showHtml = false !== $showHtml;
+    $var = var_export($var, true);
+    if (PHP_SAPI === 'cli' || $showHtml === false) {
+        $template = $text;
+        $lineInfo = sprintf('%s (line %s)', $file, $line);
+    } else {
+        $template = $html;
+        $var = htmlentities($var);
+        $lineInfo = sprintf('<span><strong>%s</strong> (line <strong>%s</strong>)</span>', $file, $line);
+    }
+
+    printf($template, $lineInfo, $var);
+}
+
+/**
+ * CakePHP's pluginSplit basic function adaptation from 2.9.0
+ *
+ * @param string $name The name you want to plugin split.
+ * @param string $plugin Optional default plugin to use if no plugin is found. Defaults to null.
+ * @return array Array with 2 indexes. 0 => plugin name, 1 => class name
+ * @see http://book.cakephp.org/2.0/en/core-libraries/global-constants-and-functions.html#pluginSplit
+ */
+function pluginSplit($name, $plugin = null)
+{
+    if (false !== strpos($name, '.')) {
+        $parts = explode('.', $name, 2);
+        return $parts;
+    }
+    return array($plugin, $name);
+}
